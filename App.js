f0e4a1ff59651fb7aac19df6826efc80352d3285
bkartyas <bkartyas@gmail.com>
2017-03-02 22:29:17 +0100

    improve UI

--- App.js
+++ App.js
@@ -6,18 +6,15 @@ var spawner = require('child_process').spawn,
 var app = express();
 var rl;
 var proc;
-//var output = '[ ';
 var output = [];
 
-/*function cacheNext(){	
-	output = '[ ';
-}*/
-
 function createInput(data){
-	var input = data['size']['x'] + ' ' + data['size']['y'] + '\n';
-	for(let i = 0; i < data['size']['x']; i++){
-		for(let j = 0; j < data['size']['y']; j++){
-			input += data['parameters'][i][j] + ' ';
+	var x = data.pitch.type.length;
+	var y = data.pitch.type[0].length;
+	var input = x + ' ' + y + '\n';
+	for(let i = 0; i < x; i++){
+		for(let j = 0; j < y; j++){
+			input += data.pitch.type[i][j] + ';' + data.pitch.reward[i][j] + ' ';
 		}
 		input += '\n';
 	}
@@ -41,31 +38,33 @@ app.post('/start', function(req, res){
 	}); rl.on('line', (line) => {
 	  	if(line && line != 'start'){
 		  	output.push(line);
-		  	//output += '"' + line.trim().split(/[ ]+/).join('", "') + '", ';
-			//console.log(line);
+			console.log("readed:" + line);
 			console.log(output);
 		}
 	});
 
-	//cacheNext();
 	res.sendStatus(200);
 })
 
 app.get("/next", function(req, res){
 	console.log("request: next");
-	//output += ']';
 	var myOut = output[0] + output[1];
 	console.log(myOut);
 	output.splice(0, 2);
-	//cacheNext();
 	res.end(myOut);
 })
 
-app.get("/a.js", function(req, res){
-	res.sendFile("D:\\Suli\\2016-17(2.)\\Onlab\\onlab\\static\\a.js");
+app.get("/index.js", function(req, res){
+	//res.sendFile("D:\\Suli\\2016-17(2.)\\Onlab\\onlab\\static\\index.js");
+	res.sendFile("C:\\Users\\Heavenly\\Desktop\\onlab\\www\\index.js");
 })
 
-app.use(express.static('static'));
+app.get("/agent.jpg", function(req, res){
+	//res.sendFile("D:\\Suli\\2016-17(2.)\\Onlab\\onlab\\static\\index.js");
+	res.sendFile("C:\\Users\\Heavenly\\Desktop\\onlab\\www\\pictures\\agent.jpg");
+})
+
+app.use(express.static('www'));
 var server = app.listen(3000, function(){
-	console.log('static');
+	console.log('www');
 })
---
+++ www/index.js
@@ -0,0 +1,117 @@
+var data =  { "pitch": { "type":   [], 
+ 			 			 "reward": [] }};
+
+function resizePitch(){
+	data.pitch.type = [];
+	data.pitch.reward = [];
+	var x = sizeX.value;
+	var y = sizeY.value;
+
+	while (pitch.firstChild) {
+	    pitch.removeChild(pitch.firstChild);
+	}
+
+	for(i = 0; i < y; i++){
+		data.pitch.type.push([]);
+		data.pitch.reward.push([]);
+		let newTR = document.createElement("tr");
+		for(j = 0; j < x; j++){
+			data.pitch.type[i].push("P");
+			data.pitch.reward[i].push("0");
+			newTR.appendChild(createTableData(j, i));
+		}
+		pitch.appendChild(newTR);	
+	}
+
+  	$("td").height($("td").width());
+}
+
+function createTableData(i, j){
+	let newTD = document.createElement("td");
+  	newTD.align = "center";
+  	newTD.style["vertical-align"] = "middle";
+	newTD.id = "pTD:" + i + ";" + j;
+  	newTD.addEventListener("click", function(event) {
+  		if(event.target.tagName === "TD"){
+    		stepStatus(event.target);
+    	}
+	});
+
+	let textBox = document.createElement("input");
+	textBox.addEventListener("keydown", function(event) {	
+		if (event.keyCode == 13) {
+			var parsedID = event.target.id.split(":")[1].split(";");
+			var x = parsedID[0];
+			var y = parsedID[1];
+
+			data.pitch.reward[y][x] = event.target.value;		
+		}
+	});
+	textBox.style["font-size"] = "80%";
+	textBox.size = "1";
+	textBox.id = "pTB:" + i + ";" + j;
+  	newTD.appendChild(textBox);
+	newTD.style.backgroundColor = "white";
+
+    //newTD.style.backgroundImage = "url('http://clipart-library.com/image_gallery/145213.jpg')";
+
+	return newTD;
+}
+
+function stepStatus(tableData){
+	var parsedID = tableData.id.split(":")[1].split(";");
+	var x = parsedID[0];
+	var y = parsedID[1];
+
+	switch(tableData.style.backgroundColor){
+	case "white":
+		tableData.style.backgroundColor = "black";  
+		data.pitch.type[y][x] = "W";
+	break;
+	case "black":
+		tableData.style.backgroundColor = "blue";
+		data.pitch.type[y][x] = "A";
+	break;
+	case "blue":
+		tableData.style.backgroundColor = "yellow";         
+		data.pitch.type[y][x] = "F";
+	break;
+	case "yellow":
+		tableData.style.backgroundColor = "white";
+		data.pitch.type[y][x] = "P";         
+	break;
+	default:
+		tableData.style.backgroundColor = "white";         
+		data.pitch.type[y][x] = "P";         
+	}
+}
+
+$(document).ready(function(){
+
+	resizePitch();
+	var nextValue;
+	
+	
+	startButton.addEventListener("click", function(){ 
+		$.ajax({
+			method: 'POST',
+			url: '/start',
+			data: data,
+			dataType: 'json',
+			success: function(data){
+				console.log(data);
+				nextValue = data;
+			}
+		});
+
+		setInterval(function(){$.ajax({
+			method: 'GET',
+			url: '/next',
+			dataType: 'json',
+			success: function(data){
+				console.log(data);
+				phar.innerHTML=data;
+		}
+	})}, 3000); });
+	
+});
